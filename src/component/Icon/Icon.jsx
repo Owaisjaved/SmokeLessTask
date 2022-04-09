db5d2e086f48812ca7df5d17e5fcb6f9c82d1008
OwaisJaved <owaisjaved954@gmail.com>
2022-04-09 18:01:20 +0200

    Icon Component

---
+++ src/component/Icon/Icon.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import "./icon.css";
+const Icon = () => {
+  return (
+    <div className="icon-container">
+      <div>
+        <img src="/asset/arrow_left.png" alt="left-icon"></img>
+      </div>
+    </div>
+  );
+};
+
+export default Icon;
