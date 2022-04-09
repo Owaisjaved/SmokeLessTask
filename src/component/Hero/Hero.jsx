bee8dfd5e5ce624838c5ff1ef69a820258f730cb
OwaisJaved <owaisjaved954@gmail.com>
2022-04-09 18:00:56 +0200

    Hero Banner

---
+++ src/component/Hero/Hero.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import "./hero.css";
+const Hero = () => {
+  return (
+    <div className="hero-component">
+      <img src="/asset/hero_image@3x.png" alt="icecream-banner"></img>
+      <p className="text-styling text-center">
+        Build up your icecream universe
+      </p>
+    </div>
+  );
+};
+
+export default Hero;
