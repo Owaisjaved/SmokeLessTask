f6267b8c9367637102aa2a921ddc09d8f164d30c
OwaisJaved <owaisjaved954@gmail.com>
2022-04-09 18:01:11 +0200

    Main Home Page Component

---
+++ src/component/Home/Home.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import Button from '../Buttons/Button'
+import Card from '../Cards/Card'
+import Hero from '../Hero/Hero'
+
+const Home = () => {
+  return (
+    <div>
+        <Hero/>
+        <Card/>
+        <Button/>
+    </div>
+  )
+}
+
+export default Home
